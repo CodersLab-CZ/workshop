8fa64c0e547c2b58fb38b555396a10638b990818
Diana Prusova <di.prusova@gmail.com>
2024-01-31 14:47:23 +0100

    first example finish

---
+++ 1] guessing game/README.md
@@ -0,0 +1,23 @@
+## GUESSING GAME
+
+Symple console application for guessing numbers. 
+
+
+# Lauched application
+
+Use the "cd" command to navigate to the "1] guessing game" folder and enter the command:
+
+for LINUX
+    python3 main.py
+
+for WINDOWS
+    python main.py
+
+
+# Running application
+
+The application generates a random number between 1 and 100. The player tries to guess this number.
+
+The application prompts the user to enter an integer. If the player enters letters or floats, the app will ask them to correct it. If the entered number is higher or lower than the selected number, the application will alert the player. When the player guesses the number, the app will tell them and the program will exit.
+
+If the player wants to quit the application during the game, he can use "CTRL+C".
--- 1] guessing game/main.py
+++ 1] guessing game/main.py
@@ -1,1 +1,38 @@
-print("Testuji propojení repozitáře.")
+from random import randint
+
+
+def num_guessing_game() -> int:
+    """
+    The function of guessing a random number between 1 and 100.
+
+    The function using randint will generate a number, and using
+    while loop accepts number tips from the user. once
+    the user guesses the number, the program ends.
+    """
+    print("Welcome to our guessing game! "
+          "Can you guess the number between 1 and 100?")
+    
+    # getting a random number
+    wanted_num = randint(1, 101)
+
+    # getting a number of player
+    while True:
+        try:
+            player_num = int(input("Enter whole number: "))
+
+            # testing a player's number
+            if player_num < wanted_num:
+                print("Too small number...")
+                continue
+            elif player_num > wanted_num:
+                print("Too big number...")
+                continue
+            else: 
+                return "!!! YOU WIN !!!"
+
+        # wrong value entery check    
+        except ValueError:
+            print("You entered the wrong value, try again...")
+
+
+print(num_guessing_game())
