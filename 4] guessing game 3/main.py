89376fce16eb66721a95f58b543f4534f8389ef8
Diana Prusova <di.prusova@gmail.com>
2024-02-02 20:00:26 +0100

    oprava čtvrtého cvičení

--- 4] guessing game 3/main.py
+++ 4] guessing game 3/main.py
@@ -4,33 +4,47 @@ app = Flask(__name__)
 
 min_num = 0
 max_num = 1000
-used_button = ""
+guess = 0
 
-@app.route("/geussing", methods=["GET", "POST"])
-def geussing(button=used_button):
-    return used_button == request.form("button")
+def calculation(minimum=int, maximum=int) -> int:
+    return int((maximum-minimum) / 2) + minimum
+
+
+@app.route("/too_low", methods=["GET", "POST"])
+def too_low():
+    global min_num, max_num, guess
+    min_num = guess
+    guess = calculation(min_num, max_num)
+    return render_template("guessing.html", guess=guess)
+
+
+@app.route("/too_high", methods=["GET", "POST"])
+def too_high():
+    global min_num, max_num, guess
+    max_num = guess
+    guess = calculation(min_num, max_num)
+    return render_template("guessing.html", guess=guess)
+
+
+@app.route("/you_win", methods=["GET", "POST"])
+def you_win():
+    global min_num, max_num, guess
+    min_num = 0
+    max_num = 1000
+    guess = 0
+
+    return render_template("i_win.html") 
 
 
 @app.route("/", methods=["GET", "POST"])
-def game(minimum=min_num, maximum=max_num):
+def game():
     if request.method == "GET":
         return render_template("index.html")
     elif request.method == "POST":
-
-
-        if used_button == "too_low":
-            minimum = request.form["guess"]
-
-        elif used_button == "too_high":
-            maximum == request.form["guess"]
-
-        elif used_button == "you_win":
-            return render_template("i_win.html")
-        else: 
-
-            guess = int((maximum-minimum) / 2) + minimum
-            return render_template("guessing.html", guess=guess)            
+        global min_num, max_num, guess
         
+        guess = calculation(min_num, max_num)
+        return render_template("guessing.html", guess=guess)   
 
 
 if __name__ == "__main__":
