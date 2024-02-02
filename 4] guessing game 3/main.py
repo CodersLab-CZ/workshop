b651e2d55957137b191747422e98d76812d53547
Diana Prusova <di.prusova@gmail.com>
2024-02-02 16:04:58 +0100

    oprava cvičení 4

--- 4] guessing game 3/main.py
+++ 4] guessing game 3/main.py
@@ -2,28 +2,34 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-minimum = 0
-maximum = 1000
+min_num = 0
+max_num = 1000
+used_button = ""
+
+@app.route("/geussing", methods=["GET", "POST"])
+def geussing(button=used_button):
+    return used_button == request.form("button")
 
 
 @app.route("/", methods=["GET", "POST"])
-def game():
+def game(minimum=min_num, maximum=max_num):
     if request.method == "GET":
         return render_template("index.html")
     elif request.method == "POST":
 
-        # used_button = request.form("button")
-        # if used_button == "too_low":
-        #     minimum = request.form["guess"]
 
-        # elif used_button == "too_high":
-        #     maximum == request.form["guess"]
+        if used_button == "too_low":
+            minimum = request.form["guess"]
 
-        # elif used_button == "you_win":
-        #     return render_template("i_win.html")
+        elif used_button == "too_high":
+            maximum == request.form["guess"]
 
-        guess = int((maximum-minimum) / 2) + minimum
-        return render_template("guessing.html", guess=guess)            
+        elif used_button == "you_win":
+            return render_template("i_win.html")
+        else: 
+
+            guess = int((maximum-minimum) / 2) + minimum
+            return render_template("guessing.html", guess=guess)            
         
 
 
